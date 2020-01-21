b17c265a44e021ec7f17f69ad65c2647d22f288b
Héber de Castro <heber_bega@hotmail.com>
2020-01-21 08:46:16 -0300

    First commit - add -all

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+    @Component({
+        selector: 'meu-primeiro-component', 
+        template: "<h1>Acabo de criar meu primeiro Component - Héber</h1>"
+    })
+    export class MeuPrimeiroComponent { }
